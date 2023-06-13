f8913177df295baff83f686023f94b5ceb0da710
PurosovMV <mihail.purosov@gmail.com>
2023-06-13 21:44:48 +0300

    correction main()

--- Main.java
+++ Main.java
@@ -12,10 +12,10 @@ public class Main {
         Student student3 = new Student("Кристина Клименко", 2001);
         List<Student> firstGroup = new ArrayList<>(Arrays.asList(student1, student2, student3));
 
-        Teacher teacher2 = new Teacher("Виктор Гаврилов", 1933, Disciple.PROGRAMMING);
-        Student student4 = new Student("Галина Ковальская", 1962);
-        Student student5 = new Student("Сергей Федюков", 1940);
-        Student student6 = new Student("Сергей Чижиков", 1963);
+        Teacher teacher2 = new Teacher("Виктор Гаврилов", 1959, Disciple.PROGRAMMING);
+        Student student4 = new Student("Галина Ковальская", 2000);
+        Student student5 = new Student("Сергей Федюков", 2003);
+        Student student6 = new Student("Сергей Чижиков", 2002);
         List<Student> secondGroup = new ArrayList<>(Arrays.asList(student4, student5, student6));
 
         StudyGroup group1 = controller.createGroup(firstGroup, oneteacher);
